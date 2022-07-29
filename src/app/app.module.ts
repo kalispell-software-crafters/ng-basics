93da2e4a3eea3f722b9afa659388a13683cbad26
Travis Craft <tlcraft@users.noreply.github.com>
2022-07-29 17:59:59 -0600

    Add counter display.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterDisplayComponent } from './counter-display/counter-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterDisplayComponent
   ],
   imports: [
     BrowserModule,
