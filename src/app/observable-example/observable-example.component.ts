01774457bd00cb849765dd4109cab9fa5dfbcfec
Travis Craft <tlcraft@users.noreply.github.com>
2022-08-02 11:14:25 -0600

    Add map and pipe example.

--- src/app/observable-example/observable-example.component.ts
+++ src/app/observable-example/observable-example.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, pipe } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { ObservableService } from 'src/services/observable.service';
 
 @Component({
@@ -15,17 +16,31 @@ export class ObservableExampleComponent{
     // setTimeout(() => { observer.error(new Error("Test error!"))}, 1000); // Once we call error no more data will be processed and complete will not be called
     observer.next(3);
     setTimeout(() => { observer.next(4)}, 2000);
-    setTimeout(() => { observer.complete() }, 3000); // Once we call complete no more data will be processed
+    setTimeout(() => { observer.next(55)}, 5000);
+    setTimeout(() => { observer.complete() }, 5500); // Once we call complete no more data will be processed
   });
 
   constructor(private observableService: ObservableService) { 
-    this.simpleObservable.subscribe(
-      (incomingValue: number) => {
+    const observer = {
+      next: (incomingValue: number) => {
         console.log("Incoming value is: ", incomingValue);
       },
-      (error) => console.error(error.message),
-      () => console.log("The observable has completed emitting values.")
-    );
+      error: (error) => console.error(error.message),
+      compelte: () => console.log("The observable has completed emitting values.")
+    }
+
+    const simpleObserver = this.simpleObservable.subscribe(observer);
+
+    setTimeout(() => simpleObserver.unsubscribe(), 4000);
+    // setTimeout(() => {
+    //   const anotherSimpleObserver = this.simpleObservable.subscribe(
+    //     (incomingValue: number) => {
+    //       console.log("* Incoming value is: ", incomingValue);
+    //     },
+    //     (error) => console.error(error.message),
+    //     () => console.log("* The observable has completed emitting values.")
+    //   );
+    // }, 2000);
   }
 
   getStream(): string {
@@ -39,5 +54,18 @@ export class ObservableExampleComponent{
 
   addToStream(num: string): void {
     this.observableService.addToStream(+num);
+  }
+
+  mapExample(): void {
+    // See https://angular.io/guide/rx-library
+    const nums = of(1, 2, 3);
+
+    const squareValues = pipe(
+      map((val: number) => val * val), 
+      map((val: number) => val + 1)
+    );
+    const squaredNums = squareValues(nums);
+
+    squaredNums.subscribe(x => console.log("Map example: " + x));
   }
 }
